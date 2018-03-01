c51abb1e4de79ffb6cbc7d6bedfb8cdc31c9ba40
Catarina SC <catarsc@gmail.com>
2018-03-01 19:10:05 +0000

    Initial commit

---
+++ app/src/main/java/com/example/catarina/literaryquiz/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.catarina.literaryquiz;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView name;
+    TextView questionOneAnswerThree;
+    TextView questionTwoAnswerTwo;
+    TextView questionTwoAnswerFour;
+    TextView questionThreeAnswerOne;
+    TextView questionThreeAnswerTwo;
+    TextView questionThreeAnswerFour;
+    TextView questionFourAnswerThree;
+    TextView questionFiveAnswer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView picture = (ImageView) findViewById(R.id.picture);
+        picture.setImageResource(R.drawable.books);
+        name = findViewById(R.id.name);
+        questionOneAnswerThree = findViewById(R.id.question_one_answer_three);
+        questionTwoAnswerTwo = findViewById(R.id.question_two_answer_two);
+        questionTwoAnswerFour = findViewById(R.id.question_two_answer_four);
+        questionThreeAnswerOne = findViewById(R.id.question_three_answer_one);
+        questionThreeAnswerTwo = findViewById(R.id.question_three_answer_two);
+        questionThreeAnswerFour = findViewById(R.id.question_three_answer_four);
+        questionFourAnswerThree = findViewById(R.id.question_four_answer_three);
+        questionFiveAnswer = findViewById(R.id.question_five_answer);
+    }
+
+    /**
+     * This method is called when the Submit button is clicked.
+     */
+
+    public void checkAnswers(View view) {
+        // Get user's name
+        EditText nameField = (EditText) name;
+        Editable nameEditable = nameField.getText();
+        String user = nameEditable.toString();
+
+        //Answers to the questions
+        //Answer One. RadioButton
+        RadioButton questionOneAnswerThreeRadioButton = (RadioButton) questionOneAnswerThree;
+        boolean isQuestionOneAnswerThree = questionOneAnswerThreeRadioButton.isChecked();
+
+        //Answer Two. CheckBox
+
+        CheckBox questionTwoAnswerTwoCheckBox = (CheckBox) questionTwoAnswerTwo;
+        boolean isQuestionTwoAnswerTwo = questionTwoAnswerTwoCheckBox.isChecked();
+        CheckBox questionTwoAnswerFourCheckBox = (CheckBox) questionTwoAnswerFour;
+        boolean isQuestionTwoAnswerFour = questionTwoAnswerFourCheckBox.isChecked();
+        boolean isQuestionTwoTotal = isQuestionTwoAnswerTwo && isQuestionTwoAnswerFour;
+
+        //Answer Three. CheckBox
+
+        CheckBox questionThreeAnswerOneCheckBox = (CheckBox) questionThreeAnswerOne;
+        boolean isQuestionThreeAnswerOne = questionThreeAnswerOneCheckBox.isChecked();
+        CheckBox questionThreeAnswerTwoCheckBox = (CheckBox) questionThreeAnswerTwo;
+        boolean isQuestionThreeAnswerTwo = questionThreeAnswerTwoCheckBox.isChecked();
+        CheckBox questionThreeAnswerFourCheckBox = (CheckBox) questionThreeAnswerFour;
+        boolean isQuestionThreeAnswerFour = questionThreeAnswerFourCheckBox.isChecked();
+        boolean isQuestionThreeTotal = isQuestionThreeAnswerOne && isQuestionThreeAnswerTwo && isQuestionThreeAnswerFour;
+
+        //Answer Four. RadioButton
+
+        RadioButton questionFourAnswerThreeRadioButton = (RadioButton) questionFourAnswerThree;
+        boolean isQuestionFourAnswerThree = questionFourAnswerThreeRadioButton.isChecked();
+
+        //Answer Five. EditText
+
+        EditText questionFiveAnswerEditText = (EditText) questionFiveAnswer;
+        String questionFiveAnswer = questionFiveAnswerEditText.getText().toString();
+        boolean isQuestionFiveAnswer = questionFiveAnswer.equalsIgnoreCase("Moçambique") || questionFiveAnswer.equalsIgnoreCase("Moçambique ") || questionFiveAnswer.equalsIgnoreCase("Mozambique ") || questionFiveAnswer.equalsIgnoreCase("Mozambique");
+
+        //Toast Message with the results
+
+        Context context = getApplicationContext();
+        CharSequence text = createAnswerSummary(user, isQuestionOneAnswerThree, isQuestionTwoTotal, isQuestionThreeTotal, isQuestionFourAnswerThree, isQuestionFiveAnswer);
+        int duration = Toast.LENGTH_SHORT;
+        Toast finalResult = Toast.makeText(context, text, duration);
+        finalResult.show();
+    }
+
+    //Counts the right answers
+
+    public String createAnswerSummary(String user, boolean isQuestionOneAnswerThree, boolean isQuestionTwoTotal, boolean isQuestionThreeTotal, boolean isQuestionFourAnswerThree, boolean isQuestionFiveAnswer) {
+
+        int counter = 0;
+
+        if (isQuestionOneAnswerThree) {
+            counter = counter + 1;
+        }
+
+        if (isQuestionTwoTotal) {
+            counter = counter + 1;
+        }
+
+        if (isQuestionThreeTotal) {
+            counter = counter + 1;
+        }
+
+        if (isQuestionFourAnswerThree) {
+            counter = counter + 1;
+        }
+
+        if (isQuestionFiveAnswer) {
+            counter = counter + 1;
+        }
+
+        String quizResultsMessage = getString(R.string.user_name, user);
+        quizResultsMessage += "\n\n" + getString(R.string.counter, counter);
+        quizResultsMessage += "\n\n" + getString(R.string.thank_you);
+        return quizResultsMessage;
+    }
+}
